9d6a5d75e41015ffad9cb6fc4c714bed59e91f2e
TheLunix <myagkohleb.f@gmail.com>
2025-11-09 17:20:45 +0200

    Auto update 09.11.2025 17:20:45,74

--- update_github.bat
+++ update_github.bat
@@ -1,11 +1,13 @@
-git config --global --add safe.directory "D:/Farm Clash/FarmClash"
 @echo off
 REM === Налаштування ===
-set PROJECT_PATH=D:\Farm Clash\FarmClash
-set COMMIT_MSG=Auto update %date% %time%
+set "PROJECT_PATH=D:\Farm Clash\FarmClash"
+set "COMMIT_MSG=Auto update %date% %time%"
 
 REM === Перехід у папку проєкту ===
 cd /d "%PROJECT_PATH%"
+
+REM === Додаємо теку у безпечний список Git ===
+git config --global --add safe.directory "D:/Farm Clash/FarmClash"
 
 REM === Додавання всіх змін ===
 git add .
